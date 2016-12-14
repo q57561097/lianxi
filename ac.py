ba1d65973c0b3ef7df70d42ab84e5d2ed0ef8810
q57561097 <929248901@qq.com>
2016-12-14 10:59:31 +0800

    转账成功

--- ac.py
+++ ac.py
@@ -1,15 +1,14 @@
 #coding:utf-8
 import mysql.connector
-source_acctid=input('甲方id:')
-target_acctid=input('乙方id:')
-money=input('借款金额:')
+
+
 class TransferMoney(object):
     def __init__(self,conn):
         self.conn=conn
     def check_acct_available(self,acctid):
-        cursor=self.conn.cursor
+        cursor=self.conn.cursor()
         try:
-            sql="select*from yh where acctid=%"% acctid
+            sql="select * from sf where acctid=%s"% acctid
             cursor.execute(sql)
             print("check_acct_available:"+sql)
             rs=cursor.fetchall()
@@ -18,9 +17,9 @@ class TransferMoney(object):
         finally:
             cursor.close
     def has_enough_money(self,acctid,money):
-        cursor = self.conn.cursor
+        cursor = self.conn.cursor()
         try:
-            sql = "select*from yh where acctid=% and money>%s" % (acctid,money)
+            sql = "select * from sf where acctid=%s and money>%s" % (acctid,money)
             cursor.execute(sql)
             print("has_enough_money:"+sql)
             rs = cursor.fetchall()
@@ -28,24 +27,24 @@ class TransferMoney(object):
                 raise Exception("账号&s没有足够的钱" % acctid)
         finally:
             cursor.close
-    def reduce_money(self,acctid,money):
-        cursor = self.conn.cursor
+    def reduce_money(self,money,acctid):
+        cursor = self.conn.cursor()
         try:
-            sql = "update yh set money=money-%s where acctid=%s" % (acctid, money)
+            sql = "update sf set money=money-%s where acctid=%s" % ( money,acctid)
             cursor.execute(sql)
             print("reduce_money:" + sql)
-            rs = cursor.fetchall()
+            #rs = cursor.fetchall()
             if cursor.rowcount != 1:
                 raise Exception("账号&s减款失败" % acctid)
         finally:
             cursor.close
-    def add_money(self,acctid,money):
-        cursor = self.conn.cursor
+    def add_money(self,money,acctid):
+        cursor = self.conn.cursor()
         try:
-            sql = "update yh set money=money+%s where acctid=%s" % (acctid, money)
+            sql = "update sf set money=money+%s where acctid=%s" % ( money,acctid)
             cursor.execute(sql)
             print("add_money:" + sql)
-            rs = cursor.fetchall()
+            #rs = cursor.fetchall()
             if cursor.rowcount != 1:
                 raise Exception("账号&s加款失败" % acctid)
         finally:
@@ -55,8 +54,8 @@ class TransferMoney(object):
             self.check_acct_available(source_acctid)
             self.check_acct_available(target_acctid)
             self.has_enough_money(source_acctid,money)
-            self.reduce_money(source_acctid,money)
-            self.add_money(target_acctid,money)
+            self.reduce_money(money,source_acctid)
+            self.add_money(money,target_acctid)
             self.conn.commit()
         except Exception as e:
             self.conn.rollback()
@@ -66,11 +65,14 @@ class TransferMoney(object):
 
 
 if __name__=="__main__":
-    conn=mysql.connector.connect(user = 'root', passwd = 'password',database='yh')
+    source_acctid = input("请输入减款人： ")
+    target_acctid = input("请输入收款人： ")
+    money = input("请输入转款数： ")
+    conn=mysql.connector.connect(user = 'root', passwd = 'password',database='ph')
     tr_money=TransferMoney(conn)
     try:
         tr_money.transfer(source_acctid,target_acctid,money)
-    except Exception as  e:
+    except Exception as e:
         print("出现问题"+str(e))
     finally:
         conn.close()
--- test.py
+++ test.py
@@ -1,7 +1,8 @@
 import mysql.connector
-conn = mysql.connector.connect( user = 'root', passwd = 'password')
+conn = mysql.connector.connect( user = 'root', passwd = 'password',)
 cursor=conn.cursor()
 try:
-    cursor.execute('create database yh')
+    cursor.execute('create database ph')
 except:
     print('Database addtest exists!')
+cursor.execute('create table sf (acctid int(11) primary key, money int(11))')
